23ac9b762e50d4ef68fbc2436dee83bea075d1a4
clvr024 <clvr024@gmail.com>
2024-08-06 14:30:04 +0800

    add env.HOSTNAME

--- _worker.js
+++ _worker.js
@@ -79,7 +79,7 @@ export default {
 					default:
 						// return new Response('Not found', { status: 404 });
 						// For any other path, reverse proxy to 'ramdom website' and return the original response, caching it in the process
-						const randomHostname = cn_hostnames[Math.floor(Math.random() * cn_hostnames.length)];
+						const randomHostname = env.HOSTNAME || cn_hostnames[Math.floor(Math.random() * cn_hostnames.length)];
 						const newHeaders = new Headers(request.headers);
 						newHeaders.set('cf-connecting-ip', '1.2.3.4');
 						newHeaders.set('x-forwarded-for', '1.2.3.4');
